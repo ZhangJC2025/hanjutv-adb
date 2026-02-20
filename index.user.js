101b1b89b06230e080355999570f434e32aee0fe
ZhangJC <zhangjc@gmail.com>
2026-02-20 11:43:20 +0800

    change delay

--- index.user.js
+++ index.user.js
@@ -1,24 +1,21 @@
 // ==UserScript==
 // @name         hanju-adb
 // @namespace    https://github.com/ZhangJC2025/hanjutv-adb/
-// @version      2026-02-20
+// @version      1.0.1
 // @description  fuck hanju1.tv's adult ads
 // @author       ZhangJC
-// @match        https://hanju1.tv/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=hanju1.tv
+// @match        *://hanju1.tv/*
+// @icon         https://youke.xn--y7xa690gmna.cn/s1/2026/02/20/6997d69af32ab.ico
+// @license      MIT
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
-    document.head.appendChild(document.createElement('style')).textContent = `[style*="z-index: 2147483646"] { display: none !important; }`;
-
-    setInterval(() => {
-        document.querySelectorAll('*').forEach(el => {
-            if (getComputedStyle(el).zIndex === '2147483646') {
-                el.remove();
-            }
-        });
-    }, 100);
+ 
+    function blockAds() {
+        document.querySelectorAll('*').forEach(x=> {if (getComputedStyle(x).zIndex === '2147483646') {x.remove();}})
+    }
+ 
+    setInterval(blockAds, 1000);
 })();
